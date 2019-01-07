adf3e4c5fe46fc87326e4e826c18c491d64f3c0e
Chris Barber <christopher.l.barber@gmail.com>
2019-01-07 16:14:08 -0700

    ID1 complete

--- problems/id1/main.go
+++ problems/id1/main.go
@@ -1,9 +1,29 @@
 package main
 
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
 import (
 	"fmt"
 )
 
+var mult = []int{}
+
 func main() {
-		
+	t:=0
+	findMultiples(1000)
+	for _, m := range mult {
+		t += m	
+	}
+
+	fmt.Println(t)
+}
+
+func findMultiples(to int) {
+	m1 := 3
+	m2 := 5
+	for i := 1; i < to; i++ {
+		if i%m1 == 0 || i%m2 == 0 {
+			mult = append(mult, i)
+		}
+	}
 }
