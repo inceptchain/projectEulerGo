a5ea4858bae5282dae69c62be21f98be18c85a3e
Chris Barber <christopher.l.barber@gmail.com>
2019-01-10 11:30:49 -0700

    increased execution speed to <4sec

--- problems/id10/main.go
+++ problems/id10/main.go
@@ -1,30 +1,35 @@
 package main 
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	max := 2000000
 	value := 2
 	sum := 0
 	sp := []int{}
+	sqrt := 0
 	for value <= max {
-		for i:=2; i<value; i++ {
-			if value%i == 0 && i != 1 && i != value {
-				value++
-				goto end
-			} 
+		sqrt = int(math.Sqrt(float64(value)))
+		for i:=1; i<value; i++ {
+			if value%i == 0 && i != 1{
+				break
+			}
+
+			if i > sqrt || value == 2{
+				sp = append(sp, value)
+				fmt.Println(value)
+				break
+			}
 		}
-		sp = append(sp, value)
-		fmt.Println(value)
 		value++
-		end:
 	}
-	
 
-
-		for _, v := range sp {
-			sum += v
-		}
+	for _, v := range sp {
+		sum += v
+	}
 
 	fmt.Println(sum)
 }
