39d03258eaa34b6782075195cb519682c47ed78a
Chris Barber <christopher.l.barber@gmail.com>
2019-01-16 16:41:20 -0700

    solved problem 12

--- problems/id12/main.go
+++ problems/id12/main.go
@@ -2,50 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	terms := []int{}
-	length := 0
-	divisors := 4
-
-	createTerms(25, &terms)
-
-	for i:= 1; length <= divisors; i++ {
+	max := 0
+	createTerms(13000, &terms)
 		
-		temp := 0
-		//fmt.Println(result)
-
-		for _, v := range terms {
-			temp = getFactors(v)
+	for i:=0; i<len(terms); i++ {
+		count := []int{}
+		sqrt := math.Sqrt(float64(terms[i]))
+		for j:=1.0; j<=sqrt; j++ {
+			if terms[i]%int(j) == 0 {
+				a := terms[i]/int(j)
+				if a == int(j) {
+					count = append(count, a)
+				} else {
+					count = append(count, a, int(j))	
+				}	
+			}
 		}
+		if len(count) > max {
+			max = len(count)
+			fmt.Println(count[0])
 
-		if temp > length {
-			length = temp
-			fmt.Printf("Length: %v\n", length)
-			fmt.Printf("Value: %v\n", terms[len(terms)-1])
 		}
-
 	}
-	//fmt.Println(length)
-}
-
-func getFactors(value int) int{
-	factors := []int{}
-	for i := 1; i <= value; i++ {
-		if value%i == 0 {
-			factors = append(factors, i)
-			fmt.Println(i)
-		}
-		//fmt.Println(factors)	
-	}
-	return len(factors)
+	fmt.Println(max)
 }
 
 func createTerms(value int, slice *[]int) {
-	num := 0
 	for i:=1; i<=value; i++ {
-		num += i
+		num := 0
+		num = i*(i+1)/2
 		*slice = append(*slice, num)
+		//fmt.Println()
 	}
 }
