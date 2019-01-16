82998a08c5aaa0758aabc8d39e3c781cb2618cd5
Chris Barber <christopher.l.barber@gmail.com>
2019-01-16 14:49:47 -0700

    refactored

--- problems/id12/main.go
+++ problems/id12/main.go
@@ -6,29 +6,46 @@ import (
 
 func main() {
 	terms := []int{}
+	length := 0
+	divisors := 4
 
-	result := createTerms(10, &terms)
-	fmt.Println(result)
+	createTerms(25, &terms)
 
-	for _, v := range terms {
-		fmt.Println(v)
-	}
-
-
-}
-
-func getFactors(value int) {
-	
-	for i := 1; i <= value; i++ {
+	for i:= 1; length <= divisors; i++ {
 		
+		temp := 0
+		//fmt.Println(result)
+
+		for _, v := range terms {
+			temp = getFactors(v)
+		}
+
+		if temp > length {
+			length = temp
+			fmt.Printf("Length: %v\n", length)
+			fmt.Printf("Value: %v\n", terms[len(terms)-1])
+		}
+
 	}
+	//fmt.Println(length)
 }
 
-func createTerms(value int, slice *[]int) int{
+func getFactors(value int) int{
+	factors := []int{}
+	for i := 1; i <= value; i++ {
+		if value%i == 0 {
+			factors = append(factors, i)
+			fmt.Println(i)
+		}
+		//fmt.Println(factors)	
+	}
+	return len(factors)
+}
+
+func createTerms(value int, slice *[]int) {
 	num := 0
 	for i:=1; i<=value; i++ {
 		num += i
 		*slice = append(*slice, num)
 	}
-	return num
 }
