7b3b728fb8e222d4f7a29b37d5abca4644d6c479
Chris Barber <christopher.l.barber@gmail.com>
2019-01-18 16:47:08 -0700

    initial commit on 15

---
+++ problems/id15/main.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+	//track coordinates
+	//run through each combination, removing duplicate combinations.
+	for x := 0; x<20; x++ {
+		for y := 0; y<20; y++ {
+
+		}
+	} 
+			
+	
+}
