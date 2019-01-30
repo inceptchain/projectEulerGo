325e0f92bfb880d31659fb5766628d99838a5fdf
Chris Barber <christopher.l.barber@gmail.com>
2019-01-30 10:03:17 -0700

    solved problem 15

--- problems/id15/main.go
+++ problems/id15/main.go
@@ -1,17 +1,34 @@
-package main 
+package main
 
 import (
 	"fmt"
 )
 
 func main() {
-	//track coordinates
-	//run through each combination, removing duplicate combinations.
-	for x := 0; x<20; x++ {
-		for y := 0; y<20; y++ {
+	//si := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	gridSize := 21
+	grid := make([][]int, gridSize)
+	// for i := range grid {
+	// 	grid[i] = si[:gridSize]
+	// }
+	//fmt.Println(len(grid))
 
+	for y := 0; y < gridSize; y++ {
+		grid[y] = make([]int, gridSize)
+		for x := 0; x < gridSize; x++ {
+
+			if x == 0 || y == 0 {
+				grid[y][x] = 1
+
+			} else {
+				grid[y][x] = grid[y][x-1] + grid[y-1][x]
+			}
+
+			if x == y {
+				fmt.Println(grid[y][x])
+			}
 		}
-	} 
-			
-	
+
+	}
+
 }
