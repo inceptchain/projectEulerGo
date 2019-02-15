5cfc54d7df441658b26b7632c487a84b685e8a0b
Chris Barber <christopher.l.barber@gmail.com>
2019-02-15 12:08:33 -0700

    solved problem 19

--- problems/id19/main.go
+++ problems/id19/main.go
@@ -9,12 +9,12 @@ func main() {
 	startYear := 1901
 	endYear := 2000 
 	tSundays := 0
-	mDays := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-	wDays := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+	wDays := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",}
 	for y := startYear; y <= endYear; y++ {
 
+		mDays := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,}
 
-		if y % 4 == 0 && y%100 != 0{
+		if y%4 == 0 && y%100 != 0{
 			mDays[1] = 29 
 		}
 
@@ -22,16 +22,14 @@ func main() {
 			mDays[1] = 29
 		}
 
-		if mDays[1] == 29 {
-			startWDay++	
-		}
-
 		for _, v := range mDays{
+			
 			for i := 1; i<=v; i++ {
+
 				if i == 1 && wDays[startWDay] == "Sunday" {
 					tSundays ++
 				}
-				if startWDay == 6 {
+				if startWDay >= 6{
 					startWDay = 0
 				}else{
 					startWDay ++
