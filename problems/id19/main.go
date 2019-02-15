6d70394dbbd1dafc199422df7fd325f8ba6c1eb2
Chris Barber <christopher.l.barber@gmail.com>
2019-02-15 11:36:55 -0700

    got logic added fixing bugs

--- problems/id19/main.go
+++ problems/id19/main.go
@@ -8,25 +8,36 @@ func main() {
 	startWDay := 1
 	startYear := 1901
 	endYear := 2000 
-	tDays := 0
+	tSundays := 0
 	mDays := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 	wDays := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
 	for y := startYear; y <= endYear; y++ {
+
+
 		if y % 4 == 0 && y%100 != 0{
-			mDays[1] = 29
-			startWDay++ 
+			mDays[1] = 29 
 		}
 
 		if y%400 == 0 {
 			mDays[1] = 29
-			startWDay++
 		}
 
-		for _, v := range mdays{
-			tDays += v
+		if mDays[1] == 29 {
+			startWDay++	
 		}
 
-		for 
-
+		for _, v := range mDays{
+			for i := 1; i<=v; i++ {
+				if i == 1 && wDays[startWDay] == "Sunday" {
+					tSundays ++
+				}
+				if startWDay == 6 {
+					startWDay = 0
+				}else{
+					startWDay ++
+				}
+			}
+		}
+		fmt.Println(tSundays)
 	}	
 }
